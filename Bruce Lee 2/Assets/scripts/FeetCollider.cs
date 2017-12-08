358def68490c5e9b35dbc2cf33e3c94ba68a67e2
Gui - The uncloser of doors <nightbane@gmail.com>
2017-12-08 15:09:30 +0100

    Join Mona Controller and Gui work

---
+++ Bruce Lee 2/Assets/scripts/FeetCollider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeetCollider : MonoBehaviour {
+
+
+
+    private Animator animator;
+    private Rotate ropeRotator;
+    // Use this for initialization
+    void Start () {
+        
+        animator = GameObject.Find("T-Jump_Model-20171201 (1)").GetComponent<Animator>();
+        ropeRotator = GameObject.Find("RopeGravityCenter").GetComponent<Rotate>(); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider hit)
+    {
+
+        if (hit.gameObject.tag == "Rope")
+        {
+            //GetComponent<>
+            animator.SetBool("Colided", true);
+            //ropeRotator.StopRotation();
+            //Invoke("ColisionOf", 5.0f);
+        }
+
+    }
+}
---
+++ Bruce Lee 2/Assets/scripts/Rotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    private Rigidbody rb;
+    private bool start = false;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(start)
+            transform.Rotate(Vector3.forward * Time.deltaTime * 100);
+        //transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            start = !start;
+        }
+    }
+
+    public void StopRotation()
+    {
+        start = false;
+    }
+}
